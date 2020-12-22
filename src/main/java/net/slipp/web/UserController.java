97fee74bdc1541533b1080ad0326857a926d8cc2
Alex <wbyun1461@gmail.com>
2020-12-22 14:14:59 +0900

    회원가입, 사용자 목록 기능 구현

---
+++ src/main/java/net/slipp/web/UserController.java
@@ -0,0 +1,36 @@
+package net.slipp.web;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class UserController{
+	
+	//회원가입만 사용자를 저장하는 ListCollection
+	///users가 비어있어서
+	private List<User> users=new ArrayList<User>();
+	
+	@PostMapping("/create")
+	public String create(User user){
+		
+		System.out.println("user : "+user);
+		users.add(user);
+		return "redirect:/list";
+		
+	}
+	
+	@GetMapping("/list")
+	public String list(Model model) {
+		model.addAttribute("users",users);
+		return "list";
+	}
+	
+	
+	
+	
+}
